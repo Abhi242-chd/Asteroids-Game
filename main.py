89c2f8662e6b588a8806fb2d343aff7bbf6e457f
Abhi242-chd <abhishak2298@gmail.com>
2026-05-10 15:02:48 +0000

    CH3 Player L5: Groups

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock_object = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids")
@@ -23,10 +27,12 @@ def main():
                 return
         
         dt = clock_object.tick(60) / 1000
-        player.update(dt)
-
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
 
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, LINE_WIDTH
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, LINE_WIDTH
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -20,12 +20,29 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         return PLAYER_TURN_SPEED * dt
+    
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(- dt)
+
 
         if keys[pygame.K_a]:
            self.rotation -= self.rotate(dt)
 
         if keys[pygame.K_d]:
-           self.rotation += self.rotate(dt) 
+           self.rotation += self.rotate(dt)
+
+            
+
