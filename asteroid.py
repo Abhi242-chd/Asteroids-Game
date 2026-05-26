7c5815c6f4278ae7fcabcd597543cd031eb85f6d
Abhi242-chd <abhishak2298@gmail.com>
2026-05-26 02:16:31 +0000

    created the smaller asteroid

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
-
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE_SECONDS
+import random
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -12,4 +13,16 @@ class Asteroid(CircleShape):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)       
 
     def update(self, dt):
-       return self.velocity * dt 
+       return self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS: 
+            return
+        
+        velocity = random.uniform(20, 50)
+        new_astroit = self.velocity.rotate(velocity), self.radius - ASTEROID_MIN_RADIUS       
+        new_astroit2 = self.velocity.rotate(- velocity), self.radius - ASTEROID_MIN_RADIUS
+        return new_astroit, new_astroit2
+
+          
